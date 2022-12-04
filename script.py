2907c551764d74e812b6d70acc7281975bca9b83
sunshineinabagg <sunshineinabagg@yandex.ru>
2022-12-05 01:10:30 +0300

    Added argument "--filename" to pulling required packages from target file
    Similar with summary

--- script.py
+++ script.py
@@ -1,15 +1,40 @@
-# fly-admin-dhcp и isc-dhcp-server
-
+<<<<<<< HEAD
+import argparse
+=======
+>>>>>>> d680158dbc2ccaab0dfc72cf4f290d732b0dda2e
 import subprocess
 
-try:
-    packages = subprocess.run('apt list --installed >logfile.txt', shell=True)
-    list = open("logfile.txt", "r")
-    
-    if 'fly-admin-dhcp' and 'isc-dhcp-server' in list.read():
-        print('Packages are installed')
-    else:
-        print('Not found')
+if __name__ == "__main__":
 
+<<<<<<< HEAD
+    required_packages = ('fly-admin-dhcp', 'isc-dhcp-server')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", default='', action='store')
+    filename = parser.parse_args().filename
+
+    if filename != '':
+        with open(filename, 'r', newline='\n') as requirements_input:
+            required_packages = [pkg.rstrip() for pkg in requirements_input.readlines()]
+
+    create_installed_packages = subprocess.run('apt list --installed >installed-packages.txt', shell=True)
+    
+    with open('installed-packages.txt', 'r', newline='\n') as packages_input:
+        installed_packages = [pkg.rstrip() for pkg in packages_input.readlines()]
+
+    report = {package: False for package in required_packages}
+    for installed_package in installed_packages:
+        for required_package in required_packages:
+            if report[required_package]:
+                continue
+            if required_package in installed_package:
+                report[required_package] = True
+
+    report_data = '\n'.join([package + ': ' + str(report[package]) for package in report]).replace('True', 'SUCCESS').replace('False', 'FAILED')
+    print(report_data)
+
+    rm = subprocess.run('rm installed-packages.txt', shell=True)
+=======
 finally:
     rm = subprocess.run('rm logfile.txt', shell=True)
+>>>>>>> d680158dbc2ccaab0dfc72cf4f290d732b0dda2e
